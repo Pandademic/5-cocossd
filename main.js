15ac519ec42084e34cdc32c9a0c526bcd75a99a6
Pandademic <atharv.gupte510@gmail.com>
2021-12-31 18:27:54 -0500

    add 4 redirects

---
+++ main.js
@@ -0,0 +1,15 @@
+function redirect(){
+    window.location.replace("options/one/index.html");
+}
+function redirect2(){
+    window.location.replace("options/two/index.html");
+}
+function redirect3(){
+    window.location.replace("options/three/index.html");
+}
+function redirect4(){
+    window.location.replace("options/four/index.html");
+}
+function redirect5(){
+    window.location.replace("options/five/index.html");
+}
