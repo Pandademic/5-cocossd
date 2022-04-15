2e14643a968699b281078dac9aefd17de1c944f5
Atha <74566464+Pandademic@users.noreply.github.com>
2022-04-15 12:33:58 -0400

    Create home.js

---
+++ home.js
@@ -0,0 +1,12 @@
+function redirect(){
+    window.location.replace("one.html");
+}
+function redirect2(){
+    window.location.replace("two.html");
+}
+function redirect3(){
+    window.location.replace("three.html");
+}
+function redirect4(){
+    window.location.replace("index.html");
+}
