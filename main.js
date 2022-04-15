7b74505803d122a1663003d22ce516401b43ca34
Atha <74566464+Pandademic@users.noreply.github.com>
2022-04-15 12:55:32 -0400

    Update main.js

--- main.js
+++ main.js
@@ -6,14 +6,11 @@ objects = [];
 status = "";
 
 function preload(){
-  img = loadImage('dog-cat.jpg');
+  img = loadImage('img/op3.jpg');
 }
 function setup() { 
     canvas = createCanvas(380,380);
     canvas.center();
-    video=createCapture(VIDEO);
-    video.hide();
-
     objectDetector=ml5.objectDetector('cocossd',modelLoaded); 
     document.getElementById('status').innerHTML = "Status : Detecting Objects";
 }
